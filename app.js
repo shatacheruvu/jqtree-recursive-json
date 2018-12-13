97747e823bb0c727be565ab0291707e0731228a7
Shata Cheruvu <shatacheruvu@gmail.com>
2018-12-13 17:34:20 +0530

    improvements in functions

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const getJSON = uri => fetch(uri).then(response => response.json());
 const isObject = value => typeof value === "object" && value !== null;
 
-const addNode = (parent, key, value) => {
-  const child = {};
-  if (isObject(value)) {
-    child["name"] = key;
-    child["children"] = [];
+const buildNode = (key, value, hasChildren) => {
+  const node = {};
+  if (hasChildren) {
+    node["name"] = key;
+    node["children"] = [];
   } else {
-    child["name"] = key + ": " + value;
+    node["name"] = `${key}: ${value}`;
   }
-  parent.push(child);
-  console.log(parent);
+  return node;
+};
+
+const addNode = (parent, key, value) => {
+  const node = isObject(value)
+    ? buildNode(key, value, hasChildren = true)
+    : buildNode(key, value, hasChildren = false);
+  parent.push(node);
   return parent;
 };
 
-const build = (parent, data) => {
+const buildTree = (parent, data) => {
   let counter = 0;
   Object.keys(data).forEach(key => {
     parent = addNode(parent, key, data[key]);
     if (isObject(data[key])) {
       const parent_n = parent[counter]["children"];
-      parent[counter]["children"] = build(parent_n, data[key]);
+      parent[counter]["children"] = buildTree(parent_n, data[key]);
     }
     counter += 1;
   });
@@ -29,8 +35,15 @@ const build = (parent, data) => {
 
 const generateTree = async data => {
   const parent = [];
-  const tree = await build(parent, data);
-  $("#tree").tree({ data: tree, keyboardSupport: true, openedIcon: '-', closedIcon: '+'});
+  const tree = await buildTree(parent, data);
+  $("#tree").tree({
+    data: tree,
+    keyboardSupport: true,
+    openedIcon: "-",
+    closedIcon: "+"
+  });
 };
 
-getJSON("./sample.json").then(generateTree).catch(error => console.error(error));
+getJSON("./sample.json")
+  .then(generateTree)
+  .catch(error => console.error(error));
